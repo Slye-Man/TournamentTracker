49e3a4e6a1995b24bdc37e8f21dfe4b7c0da66c2
Selaelo Sebola <selaelosebola67@gmail.com>
2023-05-15 23:52:28 +0200

    Add project files.

---
+++ TournamentTracker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TournamentTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
